31e76a36e450ba3ca915b4f985dfe841997c1967
Sejas <antonio@sejas.es>
2019-02-28 11:25:17 +0000

    gluegun basic funcitonality

--- core/dist/services/index.js
+++ core/dist/services/index.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict'
 function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+  for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p]
 }
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./write-to-file"));
+Object.defineProperty(exports, '__esModule', { value: true })
+__export(require('./write-to-file'))
 //# sourceMappingURL=index.js.map
