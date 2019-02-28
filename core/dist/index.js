15eb2c1bc4845395d4fc3441f129dadc84f19b52
Sejas <antonio@sejas.es>
2019-02-28 13:30:15 +0000

    show the authorization url, and increment version

--- core/dist/index.js
+++ core/dist/index.js
@@ -64,8 +64,8 @@ debug = false, callbackGlobal = (lang) => null) => {
     This is the first time you execute this command.
     Please, be gentle and go to this URL to generate a new token.
     The token will be saved in this path "~/.woo/woo-token.json".
-    CLICK HERE --> : ${authUrl}`);
-        console.log(`Authorize this app by visiting this url: ${authUrl}`);
+    
+    CLICK HERE -->  ${authUrl}`);
         const rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout
